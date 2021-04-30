7288dd5d0d8aadfbcd61d1ceb6bb738e97eb9aff
SwapnilB31 <swapnilbhattacharjee187@gmail.com>
2021-04-30 18:54:46 +0530

    blah

--- server.js
+++ server.js
@@ -21,13 +21,17 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  const ipaddress = req.ip
+  const language = req.headers['accept-language']
+  const software = req.headers['user-agent']
+  res.json({ipaddress,language,software})
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(80, function () {
+  console.log('Your app is listening on port 80');
 });
+
